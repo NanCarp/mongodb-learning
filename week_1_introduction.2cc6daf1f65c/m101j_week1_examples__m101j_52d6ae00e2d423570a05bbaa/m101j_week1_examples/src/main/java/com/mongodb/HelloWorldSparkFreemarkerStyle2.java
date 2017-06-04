8ce729427b0cf914bee37965b45c34a076e4b97d
NanCarp <nancarpli@163.com>
2017-06-04 09:35:02 +0800

    Initial commit

---
+++ week_1_introduction.2cc6daf1f65c/m101j_week1_examples__m101j_52d6ae00e2d423570a05bbaa/m101j_week1_examples/src/main/java/com/mongodb/HelloWorldSparkFreemarkerStyle2.java
@@ -0,0 +1,41 @@
+package com.mongodb;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by NanCarp on 2017/6/3.
+ */
+public class HelloWorldSparkFreemarkerStyle2 {
+    public static void main(String[] args) {
+        final Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(
+                HelloWorldSparkFreemarkerStyle.class, "/");
+
+        Spark.get(new Route("/"){
+
+            public Object handle(final Request request, final Response response) {
+                StringWriter writer = new StringWriter();
+                try {
+                    Template helloTemplate = configuration.getTemplate("hello.ftl");
+                    Map<String, Object> helloMap = new HashMap<String, Object>();
+                    helloMap.put("name", "Freemarker");
+
+                    helloTemplate.process(helloMap, writer);
+                } catch (Exception e) {
+                    halt(500);
+                    e.printStackTrace();
+                }
+                return writer;
+            }
+        });
+    }
+}
